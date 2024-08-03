cf946d970a44b76ee09d99258de42641758f2965
coreylazenby22 <coreylazenby22@gmail.com>
2024-08-02 23:01:32 -0600

    modified:   functions.py

--- functions.py
+++ functions.py
@@ -1,33 +1,18 @@
-from typing import Tuple
-
-
-def getFirstLast() -> Tuple[str, str]:
-    first_name = input("\nEnter First Name: ").strip()
-    last_name = input("\nEnter Last Name: ").strip()
-    return first_name, last_name
-    
-
-def getName() -> str:
-    
-    first_name, last_name = getFirstLast()
-    full_name = first_name + " " + last_name
-    return full_name
-
-
-def greeting(name: str) -> None :
-    
-    try:
+class NPC:
+    def __init__(self, 
+                 name: str, 
+                 health: int, 
+                 attack: int, 
+                 defense: int):
         
-        if name.lower() == 'corey lazenby':
-            print("\nMY MAN!!")
-            
-        else:
-            print(f"\nWelcome, {name.title()}")
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.defense = defense
 
 
-    except ValueError:
-        print("\nInvalid! Try Again!")
 
 
-greeting(getName())
+
+NPC(10,5,5)
 
