d83107b415d576b63d38def31c5c5d118d9ba20f
coreylazenby22 <coreylazenby22@gmail.com>
2024-08-02 23:46:15 -0600

    deleted:    __pycache__/npc.cpython-312.pyc 	modified:   main.py 	deleted:    npc.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from npc import NPC, bestiary
-
-npc_instances = {name: NPC(name, **attributes) for name, attributes in bestiary.items()}
-
-# Display stats for all NPCs in the bestiary
-for npc_name, npc in npc_instances.items():
-    print(f"--- {npc_name} ---")
-    npc.display_stats()
-    print()
