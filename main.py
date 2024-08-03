5852703e648234795e33ec04b588f8bae41e288e
coreylazenby22 <coreylazenby22@gmail.com>
2024-08-03 07:34:58 -0600

    modified:   main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-def printGems(*gems):
-    for gem in gems:
-        print(gem)
+import random
+
+# List of all 50 states
+states = [
+    "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida",
+    "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine",
+    "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada",
+    "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma",
+    "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah",
+    "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
+]
+
+# Randomly select and print a state
+while True:
+    random_state = random.choice(states)
+    input()
+    print(random_state)
+
