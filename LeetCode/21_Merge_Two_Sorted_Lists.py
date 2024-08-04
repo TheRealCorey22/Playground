685a0e9a2eb93822129897ad5444aa1498cb5961
coreylazenby22 <coreylazenby22@gmail.com>
2024-08-04 03:41:13 -0600

    new file:   LeetCode/21_Merge_Two_Sorted_Lists.py

---
+++ LeetCode/21_Merge_Two_Sorted_Lists.py
@@ -0,0 +1,8 @@
+# Problem 21 - Merge Two Sorted Lists
+
+# Merged Two Already Sorted Linked Lists into One Sorted Linked List
+
+# Example: List1 = 1 -> 3 -> 5 and list2 = 2 -> 4 -> 6
+# Merged List = 1 -> 2 -> 3 -> 4 -> 5 -> 6
+
+
