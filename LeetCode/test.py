b250019f3c96634fb90a071fb31ca7bddc581799
coreylazenby22 <coreylazenby22@gmail.com>
2024-08-04 03:08:09 -0600

    new file:   LeetCode/test.py

---
+++ LeetCode/test.py
@@ -0,0 +1,15 @@
+# Problem 14 - Longest Common Prefix
+
+# Given a List of Words, Find the Longest Common Prefix
+
+# Example: Flow, Flower, Flight = Fl
+
+
+
+from typing import List
+
+
+"""
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+"""   
