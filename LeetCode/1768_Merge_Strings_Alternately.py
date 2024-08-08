9b987c2ce5ecfd70b32944757adf8451d43f40b3
coreylazenby22 <coreylazenby22@gmail.com>
2024-08-08 09:52:18 -0600

    Correcting Whitespace

--- LeetCode/1768_Merge_Strings_Alternately.py
+++ LeetCode/1768_Merge_Strings_Alternately.py
@@ -13,6 +13,7 @@ word2 = 'pqrx'
 
 
 class Solution:
+    
     def mergeAlternately(self, word1: str, word2: str) -> str:
 
 
@@ -47,53 +48,4 @@ class Solution:
         return word3
     
 
-
 print(Solution().mergeAlternately(word1,word2))
-
-
-
-
-
-
-
-"""
-word1 = 'abc'
-word2 = 'pqr'
-word3 = ''
-
-word1 = list(word1)
-word2 = list(word2)
-word3 = list(word3)
-
-
-word3 += word1[0]
-word1.pop(0)
-word3 += word2[0]
-word2.pop(0)
-
-
-word3 += word1[0]
-word1.pop(0)
-word3 += word2[0]
-word2.pop(0)
-
-
-word3 += word1[0]
-word1.pop(0)
-
-
-
-word1 = "".join(word1)
-word2 = "".join(word2)
-
-
-
-
-if word1:
-    word3.append(word1)
-
-if word2:
-    word3.append(word2)
-
-print(word3)
-"""
